fb8f732e9753b00d27244828bd089c2fd5cf2471
wsDesiree <110789227+wsDesiree@users.noreply.github.com>
2022-08-25 20:56:45 -0500

    Initial commit

---
+++ DOM-Manipulation/class-04-exercise-2/main.js
@@ -0,0 +1,12 @@
+countTrue([true, false, false, true, false])    //➞    2
+
+countTrue([false, false, false, false])    //➞    0
+
+countTrue([])   // ➞    0
+function countTrue (group){
+    let newArr =[];
+} for (let i =0; i < group.length ;i++){
+    if (group[i] === 'true'){
+       
+    }
+}
